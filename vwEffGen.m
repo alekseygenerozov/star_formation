533b8a4f9ef57c2c121ee165b2aeccf27c8d5511
Aleksey <aleksey.generozov@gmail.com>
2015-02-11 21:02:45 -0500

    Some changes in notation.

--- vwEffGen.m
+++ vwEffGen.m
@@ -8,37 +8,39 @@ Needs["vwEff`"]
 mbhs = 10.^Range[6., 9., 0.2]*MS; 
 mbhs = Sort[Append[mbhs, 4.87*10^7*MS]]; 
 mhalos = Mhalo /@ mbhs; 
+
+
 vweffStars = vweffStar /@ mhalos; 
 vweffMSPs = vweffMSP /@ mhalos; 
 vweffIas = vweffIa /@ mhalos; 
 
 
-tmp = (vweffTot[#1, 0.8] & ) /@ mhalos; 
+tmp = Table[vweffTot[mbhs[[i]], mhalos[[i]], 0.8], {i, 1, Length[mhalos]}]
 vwc = tmp[[1 ;; All,1]]; 
 vweffIasCorrected = tmp[[1 ;; All,2]]; 
 vweffTots = tmp[[1 ;; All,3]]; 
-tmp = (vweffTot[#1, 0.1] & ) /@ mhalos; 
+tmp = Table[vweffTot[mbhs[[i]], mhalos[[i]], 0.1], {i, 1, Length[mhalos]}]
 vwcCore = tmp[[1 ;; All,1]]; 
 vweffIasCoreCorrected = tmp[[1 ;; All,2]]; 
 vweffTotsCore = tmp[[1 ;; All,3]]; 
 
 
 \[Eta]s = \[Eta] /@ mhalos; 
-
-
 mdots = Table[mdotsol[mbhs[[i]], vweffTots[[i]], 0.8, \[Eta]s[[i]]], {i, 1, Length[mbhs]}]; 
 mdotsCore = Table[mdotsol[mbhs[[i]], vweffTotsCore[[i]], 0.1, \[Eta]s[[i]]], {i, 1, Length[mbhs]}]; 
 mdotEdds = mdotEdd /@ mbhs; 
 eddrs = mdots/mdotEdds; 
 eddrsCore = mdotsCore/mdotEdds; 
+
+
+radiiIa = Table[radiusIa[mbhs[[i]], mhalos[[i]]], {i, 1, Length[mbhs]}]; 
 mdotIas = Table[mdotIA[mbhs[[i]], radiiIa[[i]], 0.8, \[Eta]s[[i]]], {i, 1, Length[\[Eta]s]}]; 
 eddrIas = mdotIas/mdotEdds; 
-mdotInfs = \[Eta]s*(mbhs/th); 
-eddrInfs = mdotInfs/mdotEdds
-eddrMaxCools = Table[eddrMaxCool[mbhs[[i]], 0.8, \[Eta]s[[i]]], {i, 1, Length[\[Eta]s]}]
+mdotInfs = (\[Eta]s*mbhs)/th; 
+eddrInfs = mdotInfs/mdotEdds; 
+eddrMaxCools = Table[eddrMaxCool[mbhs[[i]], 0.8, \[Eta]s[[i]]], {i, 1, Length[\[Eta]s]}]; 
 
 
-radiiIa = radiusIa /@ mhalos; 
 stags = Table[rs[mbhs[[i]], vweffTots[[i]]], {i, 1, Length[mbhs]}]; 
 
 
@@ -51,12 +53,7 @@ vweffStarImps = vweffStarImp /@ timeImps;
 \[Eta]Imps = \[Eta]Imp /@ timeImps; 
 mbhsSparse = 10.^Range[6., 9., 1.]*MS; 
 eddrImps = Table[mdotsol[mbhsSparse[[j]], vweffStarImps[[i]], 0.8, \[Eta]Imps[[i]]]/mdotEdd[mbhsSparse[[j]]], {i, 1, Length[timeImps]}, 
-   {j, 1, Length[mbhsSparse]}]
-
-
-timeImps = 10.^Range[6.6, 10., 0.2]*year; 
-vweffStarImps = vweffStarImp /@ timeImps; 
-\[Eta]Imps = \[Eta]Imp /@ timeImps; 
+    {j, 1, Length[mbhsSparse]}]; 
 
 
 SetDirectory["/Users/aleksey/Second_Year_Project/star_formation"]
