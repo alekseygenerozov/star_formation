c4a600fa65d2fcd6b592142d8f216fcfac4051eb
Aleksey <aleksey.generozov@gmail.com>
2015-02-26 14:38:35 -0500

    Calculation of mdot and vws for average gammas.

--- mdotGen.m
+++ mdotGen.m
@@ -1,3 +1,5 @@
+(* ::Package:: *)
+
 #!/usr/local/bin/MathematicaScript/ -script
 
 
@@ -15,13 +17,18 @@ mbhs=etaf[[;;,1]]*MS;
 (*Total vwEff*)
 vweffTots=Import["vwSources.csv", "HeaderLines"->1][[;;,-1]]*10.^5;
 vweffTotsCore=Import["vwSourcesCore.csv", "HeaderLines"->1][[;;,-1]]*10.^5;
+vweffTotsGamma=Import["vwSourcesGamma.csv", "HeaderLines"->1][[;;,-1]]*10.^5;
 (*Ia radius*)
 radiiIa=Import["Ia.csv", "HeaderLines"->1][[;;,2]];
+
+\[CapitalGamma]s=\[CapitalGamma]fitM/@mbhs;
 
 
 mdots = Table[mdotsol[mbhs[[i]], vweffTots[[i]], 0.8, \[Eta]s[[i]]], 
     {i, 1, Length[mbhs]}]; 
-mdotCores = Table[mdotsol[mbhs[[i]], vweffTotsCore[[i]], 0.1, \[Eta]s[[i]]], 
+mdotsCore = Table[mdotsol[mbhs[[i]], vweffTotsCore[[i]], 0.1, \[Eta]s[[i]]], 
+    {i, 1, Length[mbhs]}]; 
+mdotsGamma = Table[mdotsol[mbhs[[i]], vweffTotsGamma[[i]], \[CapitalGamma]s[[i]], \[Eta]s[[i]]], 
     {i, 1, Length[mbhs]}]; 
 
 mdotEdds = mdotEdd /@ mbhs; 
@@ -32,18 +39,20 @@ mdotIasCore = Table[mdotIA[mbhs[[i]], radiiIa[[i]], 0.1, \[Eta]s[[i]]],
     {i, 1, Length[\[Eta]s]}];
 mdotIas = Table[mdotIA[mbhs[[i]], radiiIa[[i]], 0.8, \[Eta]s[[i]]], 
     {i, 1, Length[\[Eta]s]}];
+mdotIasGamma = Table[mdotIA[mbhs[[i]], radiiIa[[i]], \[CapitalGamma]s[[i]], \[Eta]s[[i]]], 
+    {i, 1, Length[\[Eta]s]}];
+
 mdotInfs = (\[Eta]s*mbhs)/th; 
 
 mdotMaxCoolsCore = Table[eddrMaxCool[mbhs[[i]], 0.1, \[Eta]s[[i]]], 
     {i, 1, Length[\[Eta]s]}]*mdotEdds;
 mdotMaxCools = Table[eddrMaxCool[mbhs[[i]], 0.8, \[Eta]s[[i]]], 
     {i, 1, Length[\[Eta]s]}]*mdotEdds;
+mdotMaxCoolsGamma = Table[eddrMaxCool[mbhs[[i]], \[CapitalGamma]s[[i]], \[Eta]s[[i]]], 
+    {i, 1, Length[\[Eta]s]}]*mdotEdds;
 
 (*rhoCusps=Table[rhoRs[mbhs[[i]], vweffTots[[i]], 0.8, etas[[i]]],{i, 1, Length[mbhs]}]
   rhoCores=Table[rhoRs[mbhs[[i]], vweffTots[[i]], 0.1, etas[[i]]],{i, 1, Length[mbhs]}]*)
-
-
-mdotMaxCoolsCore/mdotMaxCools
 
 
 hcs = Table[hc[mbhs[[i]], vweffTots[[i]], 0.8, \[Eta]s[[i]]], 
@@ -52,7 +61,18 @@ hcs = Table[hc[mbhs[[i]], vweffTots[[i]], 0.8, \[Eta]s[[i]]],
   {i, 1, Length[mbhs]}]; *)
 
 
+Needs["SigFig`"]
+
 Export["hc.csv", Transpose[{mbhs/MS, hcs}], "TableHeadings" -> {"Mbh", "Cusp"}]
 
-Export["mdots.csv", Transpose[{mbhs/MS, mdots, mdotCores, mdotIas, mdotIasCore, mdotInfs, mdotMaxCools, mdotMaxCoolsCore, LEdds}],TableHeadings->{"Mbh","Cusp","Core","Ia","IaCore","Inf","MaxCools","MaxCoolsCore","LEdds"}]
+mdotsAll=Map[OutputForm[SigForm[#,3, scientific->True]]&\
+,Transpose[{mbhs/MS, mdots, mdotsCore, mdotsGamma, mdotIas, mdotIasCore,\
+ mdotIasGamma, mdotInfs, mdotMaxCools, mdotMaxCoolsCore, mdotMaxCoolsGamma, LEdds}],2]
+
+Export["mdots.csv", mdotsAll, TableHeadings->
+{"Mbh","Cusp","Core","Gamma",\
+ "Ia","IaCore","IaGamma","Inf","MaxCools","MaxCoolsCore","MaxCoolsGamma","LEdds"}]
+
+
+
 
