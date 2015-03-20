925e265640a58c48ee4bc67703fa4af1a0d211d6
Aleksey <aleksey.generozov@gmail.com>
2015-03-19 20:08:05 -0400

    most things appear to work except mdotAcc

--- vwEff.m
+++ vwEff.m
@@ -202,10 +202,20 @@ radiusII[mbh_, \[CapitalGamma]_:1]:=radiusII[mbh, RIIsp[Mhalo[mbh]], \[CapitalGa
 mdotImp[t_]:=Abs[Mt0Fit'[t]] \[CapitalDelta]M[t] \[Mu]sal[Mt0Fit[t]]
 (*Mass shed by turn-off stars*)
 (*Mass injection per star for Moster star formation history truncated at lookback time t.*) 
+mdotSpecific[t_?NumericQ, mhalo_]:=NIntegrate[dNdtForm[z[t1],mhalo] Abs[dMt0dt[t1]] \[CapitalDelta]M[t1] \[Mu]sal[Mt0Fit[t1]],{t1,t,tmin,ttrans,tL[zu]}]\
+/NIntegrate[dNdtForm[z[tl], mhalo], {tl, t, tL[zu]}]
 (*mdotSpecific[t_?NumericQ, mhalo_]:=NIntegrate[dNdtForm[z[t1],mhalo] Abs[dMt0dt[t1]] \[CapitalDelta]M[t1] \[Mu]sal[Mt0Fit[t1]],{t1,t,tmin,ttrans,tL[zu]}]\
 /NIntegrate[dNdtForm[z[tl], mhalo], {tl, t, tL[zu]}]*)
-mdotSpecific[t_?NumericQ, mhalo_]:=NIntegrate[dNdtForm[z[t1],mhalo] Abs[dMt0dt[t1]] \[CapitalDelta]M[t1] \[Mu]sal[Mt0Fit[t1]],{t1,t,tL[zu]}]\
+mdotSpecific2[t_?NumericQ, mhalo_]:=NIntegrate[dNdtForm[z[t1],mhalo] Abs[dMt0dt[t1]] \[CapitalDelta]M[t1] \[Mu]sal[Mt0Fit[t1]],{t1,t,tL[zu]}]\
 /NIntegrate[dNdtForm[z[tl], mhalo], {tl, t, tL[zu]}]
+mdotSpecific3[t_?NumericQ, mhalo_]:=(NIntegrate[dNdtForm[z[t1],mhalo] Abs[dMt0dt[t1]] \[CapitalDelta]M[t1] \[Mu]sal[Mt0Fit[t1]],{t1,t,ttrans}(*, Method->"AdaptiveMonteCarlo"*)]+\
+NIntegrate[dNdtForm[z[t1],mhalo] Abs[dMt0dt[t1]] \[CapitalDelta]M[t1] \[Mu]sal[Mt0Fit[t1]],{t1,ttrans, tL[zu]}(*, Method->"AdaptiveMonteCarlo"*)])\
+/NIntegrate[dNdtForm[z[tl], mhalo], {tl, t, tL[zu]}]
+
+mdotSpecific4[t_?NumericQ, mhalo_]:=Piecewise[{{NIntegrate[dNdtForm[z[t1],mhalo] Abs[dMt0dt[t1]] \[CapitalDelta]M[t1] \[Mu]sal[Mt0Fit[t1]],{t1,t,tmin,ttrans,tL[zu]}]\
+, t<=tmin}, {NIntegrate[dNdtForm[z[t1],mhalo] Abs[dMt0dt[t1]] \[CapitalDelta]M[t1] \[Mu]sal[Mt0Fit[t1]],{t1,t,ttrans,tL[zu]}], t>tmin&&t<=ttrans}},\
+NIntegrate[dNdtForm[z[t1],mhalo] Abs[dMt0dt[t1]] \[CapitalDelta]M[t1] \[Mu]sal[Mt0Fit[t1]],{t1,t,ttrans,tL[zu]}]]/NIntegrate[dNdtForm[z[tl], mhalo], {tl, t, tL[zu]}]
+
 
 mdotForm[mhalo_]:=mdotSpecific[0, mhalo]*NIntegrate[dNdtForm[z[t1], mhalo], {t1, 0, tL[zu]}]
 mdotAcc[mhalo_]:=If[mhalo>mhaloAcc, NIntegrate[mdotSpecific[t1, mhalo]*dNdtAcc[z[t1], mhalo], {t1, 0., tL[3.]}],0]
@@ -222,10 +232,16 @@ enStarInt[mt0_]:=10.^enStarIntInterp[Log10[mt0]]
 I1=NIntegrate[enStar[Mstar], {Mstar,0.1*MS,MS}];
 (*Turnoff and main sequence energy injection per star for Moster star formation histories truncated at time t.*)
 edotTOSpecific[t_?NumericQ, mhalo_]:=NIntegrate[dNdtForm[z[t1], mhalo]*edotWR[t1], {t1, t, tL[zu]} ]/NIntegrate[dNdtForm[z[t1], mhalo], {t1, t, tL[zu]} ]
-edotMSSpecific[t_?NumericQ, mhalo_]:=(NIntegrate[dNdtForm[z[t1], mhalo]*enStarInt[Mt0Fit[t1]], {t1, t, tL[zu]}]) \
+
+edotMSSpecific2[t_?NumericQ, mhalo_]:=(NIntegrate[dNdtForm[z[t1], mhalo]*enStarInt[Mt0Fit[t1]], {t1, t, tL[zu]}]) \
+/NIntegrate[dNdtForm[z[t1], mhalo], {t1, t, tL[zu]}]
+edotMSSpecific3[t_?NumericQ, mhalo_]:=(enStarInt[100.*MS]*NIntegrate[dNdtForm[z[t1], mhalo], {t1, t, tmin}]+NIntegrate[dNdtForm[z[t1], mhalo]*enStarInt[Mt0Fit[t1]], {t1, tmin, tL[zu]}])\
 /NIntegrate[dNdtForm[z[t1], mhalo], {t1, t, tL[zu]}]
 
-edotMSSpecific2[t_?NumericQ, mhalo_]:=(enStarInt[100.*MS]*NIntegrate[dNdtForm[z[t1], mhalo], {t1, t, tmin}]+NIntegrate[dNdtForm[z[t1], mhalo]*enStarInt[Mt0Fit[t1]], {t1, tmin, tL[zu]},\
+(*edotMSSpecific[t_?NumericQ, mhalo_]:=(NIntegrate[dNdtForm[z[t1], mhalo]*enStarInt[Mt0Fit[t1]], {t1, t, tL[zu]},\
+Method->{"InterpolationPointsSubdivision","MaxSubregions"->1+Length[First@enStarInt["Coordinates"]]}])\
+/NIntegrate[dNdtForm[z[t1], mhalo], {t1, t, tL[zu]}]*)
+edotMSSpecific[t_?NumericQ, mhalo_]:=(NIntegrate[dNdtForm[z[t1], mhalo]*enStarInt[Mt0Fit[t1]], {t1, t, tmin, tL[zu]},\
 Method->{"InterpolationPointsSubdivision","MaxSubregions"->1+Length[First@enStarInt["Coordinates"]]}])\
 /NIntegrate[dNdtForm[z[t1], mhalo], {t1, t, tL[zu]}]
 
