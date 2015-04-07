022de99601ba37250c974dab027863bfaba80d48
Aleksey <aleksey.generozov@gmail.com>
2015-04-06 20:17:01 -0400

    root 3

--- mdotGen.m
+++ mdotGen.m
@@ -22,6 +22,7 @@ vweffTotsCore=Import["vwSourcesCore.csv", "HeaderLines"->1][[;;,-1]]*10.^5;
 vweffIasCoreCorrected=Import["vwSourcesCore.csv", "HeaderLines"->1][[;;,3]]*10.^5;
 
 vweffTotsGamma=Import["vwSourcesGamma.csv", "HeaderLines"->1][[;;,-1]]*10.^5;
+vweffIasGammaCorrected=Import["vwSourcesGamma.csv", "HeaderLines"->1][[;;,3]]*10.^5;
 (*Ia radius*)
 radiiIa=Import["Ia.csv", "HeaderLines"->1][[;;,2]];
 
@@ -29,8 +30,8 @@ radiiIa=Import["Ia.csv", "HeaderLines"->1][[;;,2]];
 rsCusp = Table[rs[mbhs[[i]], Sqrt[vweffTots[[i]]^2-vweffIasCorrected[[i]]^2], 0.8], {i, 1, Length[mbhs]}];
 rsCore = Table[rs[mbhs[[i]], Sqrt[vweffTotsCore[[i]]^2-vweffIasCoreCorrected[[i]]^2], 0.1], {i, 1, Length[mbhs]}];
 rsGamma = Table[rs[mbhs[[i]], Sqrt[vweffTotsGamma[[i]]^2-vweffIasGammaCorrected[[i]]^2], 0.1], {i, 1, Length[mbhs]}];
-hcs = Table[hc[mbhs[[i]], vweffTots[[i]], 0.8, \[Eta]s[[i]]], 
-    {i, 1, Length[mbhs]}];
+
+hcs = Table[hc[mbhs[[i]], vweffTots[[i]], 0.8, \[Eta]s[[i]]], {i, 1, Length[mbhs]}];
 hcsCore = Table[hc[mbhs[[i]], vweffTotsCore[[i]], 0.1, \[Eta]s[[i]]], 
   {i, 1, Length[mbhs]}]; 
 
@@ -41,17 +42,10 @@ hcIas=Table[hcIa[mbhs[[i]], vweffTots[[i]], radiiIa[[i]] 0.8, \[Eta]s[[i]]],
 hcIasCore=Table[hcIa[mbhs[[i]], vweffTotsCore[[i]], radiiIa[[i]] 0.1, \[Eta]s[[i]]], 
     {i, 1, Length[mbhs]}];
 hcOverall=Table[If[radiiIa[[i]]>rsCusp[[i]], hcIas[[i]], hcs[[i]]], {i, 1, Length[hcs]}]
+hcOverallCore=Table[If[radiiIa[[i]]>rsCore[[i]], hcIas[[i]], hcs[[i]]], {i, 1, Length[hcs]}]
 
 
 \[CapitalGamma]s=\[CapitalGamma]fitM/@mbhs;
-
-
-mdots = Table[mdotsol[mbhs[[i]], vweffTots[[i]], 0.8, \[Eta]s[[i]]], 
-    {i, 1, Length[mbhs]}]; 
-mdotsCore = Table[mdotsol[mbhs[[i]], vweffTotsCore[[i]], 0.1, \[Eta]s[[i]]], 
-    {i, 1, Length[mbhs]}]; 
-mdotsGamma = Table[mdotsol[mbhs[[i]], vweffTotsGamma[[i]], \[CapitalGamma]s[[i]], \[Eta]s[[i]]], 
-    {i, 1, Length[mbhs]}]; 
 
 
 mdotEdds = mdotEdd /@ mbhs; 
@@ -84,25 +78,28 @@ mdotComptonsGamma = Table[mdotCompton[mbhs[[i]], \[CapitalGamma]s[[i]], \[Eta]s[
     {i, 1, Length[\[Eta]s]}];
 
 
+mdotsCore = Table[mdotsol[mbhs[[i]], vweffTotsCore[[i]], 0.1, \[Eta]s[[i]]], 
+    {i, 1, Length[mbhs]}]; 
+mdots = Table[mdotsol[mbhs[[i]], vweffTots[[i]], 0.8, \[Eta]s[[i]]], 
+    {i, 1, Length[mbhs]}]; 
+mdotsGamma = Table[mdotsol[mbhs[[i]], vweffTotsGamma[[i]], \[CapitalGamma]s[[i]], \[Eta]s[[i]]], 
+    {i, 1, Length[mbhs]}]; 
+
+
+mdotsOverallCore = Table[mdotsol[mbhs[[i]], vweffTotsCore[[i]], 0.1, \[Eta]s[[i]]], 
+    {i, 1, Length[mbhs]}]; 
+mdotsOverall = Table[mdotsol[mbhs[[i]], vweffTots[[i]], 0.8, \[Eta]s[[i]]], 
+    {i, 1, Length[mbhs]}]; 
+mdotsOverallGamma = Table[If[rsGamma[[i]]>radiiIa[[i]], mdotsGamma[[i]], mdotIasGamma[[i]]], 
+    {i, 1, Length[mbhs]}]; 
+
+
 vwcs1=Table[vwComptonDom[mbhs[[i]], 0.1, \[Eta]s[[i]], TempC], {i, 1, Length[\[Eta]s]}];
-
-
-hcs = Table[hc[mbhs[[i]], vweffTots[[i]], 0.8, \[Eta]s[[i]]], 
-    {i, 1, Length[mbhs]}];
-hcsCore = Table[hc[mbhs[[i]], vweffTotsCore[[i]], 0.1, \[Eta]s[[i]]], 
-  {i, 1, Length[mbhs]}]; 
-
-(*Calculating heating/cooling ratio using Ia formalism. Using total heating--
-implicitly assuming heating is dominated by Ia's. Only take this when rs=rIa*)
-hcIas=Table[hcIa[mbhs[[i]], vweffTots[[i]], radiiIa[[i]] 0.8, \[Eta]s[[i]]], 
-    {i, 1, Length[mbhs]}];
-hcIasCore=Table[hcIa[mbhs[[i]], vweffTotsCore[[i]], radiiIa[[i]] 0.1, \[Eta]s[[i]]], 
-    {i, 1, Length[mbhs]}];
 
 
 Needs["SigFig`"]
 
-Export["hc.csv", Transpose[{mbhs/MS, hcs, hcsCore, hcIas, hcIasCore}], "TableHeadings" -> {"Mbh", "Cusp","Core", "Ia", "IaCore"}]
+Export["hc.csv", Transpose[{mbhs/MS, hcs, hcsCore, hcIas, hcIasCore hcOverall, hcOverallCore}], "TableHeadings" -> {"Mbh", "Cusp","Core", "Ia", "IaCore", "Overall", "OverallCore"}]
 
 mdotsAll=Map[OutputForm[SigForm[#,3, scientific->True]]&\
 ,Transpose[{mbhs/MS, mdots, mdotsCore, mdotsGamma, mdotIas, mdotIasCore,\
