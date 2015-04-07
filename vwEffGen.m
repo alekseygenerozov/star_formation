8c80fdf1135556d64cf5de8c9a59116195513d81
Aleksey <aleksey.generozov@gmail.com>
2015-04-06 21:07:03 -0400

    var name issues.

--- vwEffGen.m
+++ vwEffGen.m
@@ -1,3 +1,5 @@
+(* ::Package:: *)
+
 #!/usr/local/bin/MathematicaScript/ -script
 
 
@@ -21,20 +23,20 @@ vweffTots = tmp[[1 ;; All,5]];
 vweffIasCorrected= tmp[[1;;All,6]];
 
 tmp = Table[vweffTot[mbhs[[i]], mhalos[[i]], 0.1], {i, 1, Length[mhalos]}];
-vweffStars2=tmp[[1 ;; All,1]]; 
+vweffStars=tmp[[1 ;; All,1]]; 
 vweffMSPs=tmp[[1 ;; All,2]]; 
-vwc = tmp[[1 ;; All,3]]; 
+vwcCore = tmp[[1 ;; All,3]]; 
 vweffIas = tmp[[1 ;; All,4]]; 
-vweffTots = tmp[[1 ;; All,5]];  
-vweffIasCorrected= tmp[[1;;All,6]];
+vweffTotsCore = tmp[[1 ;; All,5]];  
+vweffIasCoreCorrected= tmp[[1;;All,6]];
 
 tmp = Table[vweffTot[mbhs[[i]], mhalos[[i]], \[CapitalGamma]s[[i]]], {i, 1, Length[mhalos]}];
 vweffStars=tmp[[1 ;; All,1]]; 
 vweffMSPs=tmp[[1 ;; All,2]]; 
-vwc = tmp[[1 ;; All,3]]; 
+vwcGamma = tmp[[1 ;; All,3]]; 
 vweffIas = tmp[[1 ;; All,4]]; 
-vweffTots = tmp[[1 ;; All,5]]; 
-vweffIasCorrected= tmp[[1;;All,6]];
+vweffTotsGamma = tmp[[1 ;; All,5]]; 
+vweffIasGammaCorrected= tmp[[1;;All,6]];
 
 
 (*Ia radii*)
@@ -46,13 +48,13 @@ radiiIa = Table[radiusIa[mbhs[[i]], mhalos[[i]]], {i, 1, Length[mbhs]}];
 
 
 SetDirectory["/Users/aleksey/Second_Year_Project/star_formation"]
-Export["vwSources.csv", Transpose[{mbhs*(10.^5/MS), vweffIas, vweffStars, vweffMSPs, vwc, vweffTots}/10.^5], 
+Export["vwSources.csv", Transpose[{mbhs*(10.^5/MS), vweffIas, vweffIasCorrected, vweffStars, vweffMSPs, vwc, vweffTots}/10.^5], 
   "TableHeadings" -> {"Mbh", "Ias", "IasCorrected", "Stars", "MSPs", "Compton", "Total"}]
 Export["vwSourcesCore.csv", Transpose[{mbhs*(10.^5/MS), vwcCore, vweffIasCoreCorrected, vweffTotsCore}/10.^5], 
   "TableHeadings" -> {"mbh", "Compton", "IasCorrected", "Total"}]
 Export["vwSourcesGamma.csv", Transpose[{mbhs*(10.^5/MS), vwcGamma, vweffIasGammaCorrected, vweffTotsGamma}/10.^5], 
   "TableHeadings" -> {"mbh", "Compton", "IasCorrected", "Total"}]
 
-Export["Ia.csv", Transpose[{mbhs/MS, radiiIa, rsCusp, rsCore, rsGamma}], "TableHeadings" -> {"Mbh", "rIa", "rsCusp", "rsCore", "rsGamma"}]
+Export["Ia.csv", Transpose[{mbhs/MS, radiiIa}], "TableHeadings" -> {"Mbh", "rIa", "rsCusp", "rsCore", "rsGamma"}]
 Export["eta.csv", Transpose[{mbhs/MS, \[Eta]s}], "TableHeadings" -> {"Mbh", "eta"}]
 
